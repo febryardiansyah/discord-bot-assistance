14af7251795add4372a8bd0ea20a31129a977a0b
febryardiansyah <febrymuhammad80@gmail.com>
2020-05-15 22:53:15 +0700

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Simple Discord Bot with python
+
+# Installing
+Python 3.5.3 or higher is required
+
+install library that i used on this project:
+# Linux/macOS
+python3 -m pip install -U discord.py
+
+# Windows
+py -3 -m pip install -U discord.py
---
+++ discordbot.py
@@ -0,0 +1,45 @@
+import discord
+import random
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='?')
+
+@client.event
+async def on_ready():
+    print('ready')
+
+#check ping
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'your ping is {round(client.latency * 1000)}ms')
+
+#clear text
+@client.command()
+async def clear(ctx,ammount=5):
+    await ctx.channel.purge(limit=ammount)
+
+#kick command
+@client.command()
+async def kick(ctx,member:discord.Member,*,reason=None):
+    await member.kick(reason=reason)
+
+#ban command
+@client.command()
+async def ban(ctx,member:discord.Member,*,reason=None):
+    await member.ban(reason=reason)
+    await ctx.send(f'Banned {member.mention}')
+
+#unban command
+@client.command()
+async def unban(ctx,*,member):
+    banned_user = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
+    for ban_entry in banned_user:
+        user = ban_entry.user
+
+        if(user.name, user.discriminator) == (member.name, member.discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'unban {user.mention}')
+            return
+            
+client.run('Njg4OTY3OTEyNDkyMzY3ODk1.Xr6f4A.nflDp4jMpdUt85Mo4yQdeAS5M8A')
